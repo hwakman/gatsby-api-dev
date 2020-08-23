91acc334ed83ba751c60bfcd0a5089c9c95e184e
hwakman <pa.chayoud_st@tni.ac.th>
2020-08-23 22:39:20 +0700

    update axios

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
+import Axios from 'axios'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [data, setData] = React.useState("")
+
+  const fetchapi = async ( e ) => {
+    const { value } = e.target
+    const { data } = await Axios.get(process.env.API_URL + value)
+    
+    if (data && Array.isArray(data)) {
+      let temp = ''
+      data.forEach(data => {
+        temp += JSON.stringify(data)
+      });
+
+      setData(temp)
+    }
+  }
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <input
+        type="text"
+        placeholder="place url"
+        onChange={fetchapi}
+      />
+      <p>
+        {data ? data : ""}
+      </p>
+    </Layout>
+  )
+}
 
 export default IndexPage
